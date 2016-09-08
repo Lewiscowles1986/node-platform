353dbe2dedc54930cc2d108369d16af16659caff
Lewis Cowles <lewiscowles@me.com>
2016-09-08 10:57:18 +0100

    commit of code

---
+++ node-platform.js
@@ -0,0 +1,60 @@
+(function() {
+
+'use strict';
+
+const NIX_OS = [
+    'aix',
+    'darwin',
+    'freebsd',
+    'linux',
+    'openbsd',
+    'sunos',
+    'android'
+];
+
+function getPlatform() {
+    return process.platform;
+}
+
+function isWin() {
+    return (getPlatform() === 'win32');
+}
+
+function isNix() {
+    return (NIX_OS.includes(getPlatform()));
+}
+
+function isMac() {
+    return (getPlatform() === 'darwin');
+}
+
+function isLinux() {
+    return (getPlatform() === 'linux');
+}
+
+function isBsd() {
+    return ((getPlatform() === 'freebsd') || (getPlatform() === 'openbsd'));
+}
+
+function isMac() {
+    return (getPlatform() === 'darwin');
+}
+
+function isAndroid() {
+    return (getPlatform() === 'android');
+}
+
+function isOS(os) {
+    return (getPlatform() === os);
+}
+
+module.exports.getPlatform = getPlatform;
+module.exports.isWin = isWin;
+module.exports.isNix = isNix;
+module.exports.isMac = isMac;
+module.exports.isBsd = isBsd;
+module.exports.isLinux = isLinux;
+module.exports.isAndroid = isAndroid;
+module.exports.isOS = isOs;
+
+})();
